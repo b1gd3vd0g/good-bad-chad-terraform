f6e1ba2c29fd0ce6c418b9a5447c3e74e77c674a
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-26 14:35:46 -0700

    Declare all variables that will be needed for the backend project.

---
+++ backend/variables.tf
@@ -0,0 +1,8 @@
+variable "my_ip_address" {}
+variable "pg_username" {}
+variable "pg_password" {}
+variable "db_name" {}
+variable "jwt_secret" {}
+variable "api_domain_name" {}
+variable "game_domain_name" {}
+variable "route_53_hosted_zone_id" {}
